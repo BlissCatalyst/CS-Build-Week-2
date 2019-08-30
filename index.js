41b4bf3ca58f9d8cb2c2e31ace1ea6d9f36606c9
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-08-30 10:14:32 -0600

    where I got to

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const server = require("./server.js");
+const axios = require("axios");
+
+roomGraph = {}
+
+axios
+  .create({
+    headers: {
+      "Content-Type": "application.json",
+      Authorization: "Token 0b83ec38bbe0de64cc61e9d19e649f1db870d0b9"
+    }
+  })
+  .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/move/", {
+    direction: "s"
+  })
+  .then(res => {
+    console.log(res.data);
+    roomGraph[res.data.room_id] = {}
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 *** \n");
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const axios = require("axios");
 
 const server = express();
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
+  // axios
+  //   .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/move/", {
+  //     params: {
+  //       direction: "n"
+  //     }
+  //   })
+  //   .then(res => {
+  //     console.log(res);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
   res.send("<h1>Server is Working!</h1>");
 });
 
